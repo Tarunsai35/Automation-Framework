229a9ae7956930c61578941c9f89cfb6d03fece9
Tarun <tarun@bizionictec.com>
2026-03-02 17:30:05 +0530

    add remaning methods in action class

--- Automation-Framework/src/main/java/com/orangehrm/actiondriver/ActionDriver.java
+++ Automation-Framework/src/main/java/com/orangehrm/actiondriver/ActionDriver.java
@@ -3,46 +3,73 @@ package com.orangehrm.actiondriver;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class ActionDriver {
+import com.orangehrm.base.ConfigReader;
+
+public class ActionDriver  {
 
 	private WebDriver driver;
 	private WebDriverWait wait;
 
 	public ActionDriver(WebDriver driver) {
 		this.driver = driver;
-		this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		String implicitWaitStr = ConfigReader.getProperty("implicitWait");
+		int implicitWait = implicitWaitStr != null ? Integer.parseInt(implicitWaitStr) : 30;
+		this.wait = new WebDriverWait(driver, Duration.ofSeconds(implicitWait));
 	}
-	
-	//Method to get text from an input field
+
+	// Method to Click an element
+	public void click(By by) {
+		try {
+			waitForElementToBeClick(by);
+			driver.findElement(by).click();
+		} catch (Exception e) {
+			System.out.println("Unable to Click Element:" + e.getMessage());
+		}
+	}
+
+	// Method to enter text into an input field
+	public void enterText(By by, String value) {
+		try {
+			waitForElementToBeVisible(by);
+			driver.findElement(by).clear();
+			driver.findElement(by).sendKeys(value);
+		} catch (Exception e) {
+			System.out.println("Unable to enter the value:" + e.getMessage());
+		}
+	}
+
+	// Method to get text from an input field
 	public String getText(By by) {
 		try {
 			waitForElementToBeVisible(by);
 			return driver.findElement(by).getText();
 		} catch (Exception e) {
-			System.out.println("Unable to get the Text :"+e.getMessage());
+			System.out.println("Unable to get the Text:" + e.getMessage());
 			return "";
 		}
 	}
 
-	//Method to compare two Text
+	// Method to compare two Text
 	public void compareTex(By by, String Expected) {
 		try {
 			waitForElementToBeVisible(by);
 			String actualText = driver.findElement(by).getText();
-			if(Expected.equals(actualText)) {
-				System.out.println("Text are matching"+actualText+"equal"+Expected);
-			}else {
-				System.out.println("Text are not matching"+actualText+"equal"+Expected);
+			if (Expected.equals(actualText)) {
+				System.out.println("Text are matching" + actualText + "equal" + Expected);
+			} else {
+				System.out.println("Text are not matching" + actualText + "equal" + Expected);
 			}
 		} catch (Exception e) {
-			System.out.println("unable to compare text:"+e.getMessage());
+			System.out.println("unable to compare text:" + e.getMessage());
 		}
 	}
-	
+
 	// Method to check if an element is displayed
 	public boolean isDisplayed(By by) {
 		try {
@@ -57,6 +84,17 @@ public class ActionDriver {
 		} catch (Exception e) {
 			System.out.println("elemnet is not displayed :" + e.getMessage());
 			return false;
+		}
+	}
+
+	// Scroll to an element
+	public void scrollToElement(By by) {
+		try {
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			WebElement element = driver.findElement(by);
+			js.executeScript("arguments[0]", "scrollIntoView(true)", element);
+		} catch (Exception e) {
+			System.out.println("Unable to loacte element:" + e.getMessage());
 		}
 	}
 
