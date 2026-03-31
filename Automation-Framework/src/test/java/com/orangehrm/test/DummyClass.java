8f10976f0d43af531e482678f99340dff1a151e6
Tarun <tarun@bizionictec.com>
2026-03-31 17:15:59 +0530

    headless mode changes

--- Automation-Framework/src/test/java/com/orangehrm/test/DummyClass.java
+++ Automation-Framework/src/test/java/com/orangehrm/test/DummyClass.java
@@ -11,7 +11,7 @@ public class DummyClass extends BaseClass{
 	@Test
 	public void dummyclass() {
 		//First test ---testing1
-		ExtendManager.logStep("DummyTest1 Test"); //--This has been implemented in testListener
+		ExtendManager.startTest("DummyTest1 Test"); //--This has been implemented in testListener
 		String title = getDriver().getTitle();
 		ExtendManager.logStep("Verify the title");
 		System.out.println(title);
